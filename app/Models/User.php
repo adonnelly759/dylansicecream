dc3073dd9be159f8f195a40e369a5b18660d4eb8
Aaron Keel <aaron.keel@hotmail.co.uk>
2020-11-05 22:06:12 +0000

    Validation rules removed and user creation test

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,35 +5,12 @@ use \DateTime;
 
 class User extends Model
 {
-	protected $DBGroup ='user';
+	protected $DBGroup ='default';
 	protected $table = 'user';
 	protected $primarykey = 'id';
 	protected $returnType = 'array';
     protected $allowedFields = ['email', 'password', 'first_name', 'last_name', 'session'];
-    protected $validationRules    = [
-		'email' => 'required|valid_email|is_unique(user.email)',
-		'password' => 'required',
-		'first_name' => 'required',
-		'last_name' => 'required'
-	];
 
-    protected $validationMessages = [
-		'email' => [
-			'required' => '"Email" cannot be empty.', 
-			'valid_email' => 'Please enter a valid email address.',
-			'is_unique' => 'This email is already in use. Please enter a different email address.'
-			],
-		'password' => [
-			'required' => '"Password" cannot be empty.'
-			],
-		'first_name' => [
-			'required' => '"First Name" cannot be empty.'
-			],
-		'last_name' => [
-			'required' => '"Last Name" cannot be empty.'
-			]
-	];
-    protected $skipValidation = false;
 
 	public function create_user($email, $password, $first_name, $last_name){
 		$data = [
