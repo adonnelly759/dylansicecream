ee7c1a792b5cb93f0bc4ea3628219144399abf04
Aidan <adonnelly759@qub.ac.uk>
2020-11-06 22:57:30 +0000

    Fix for isLogged not defined

--- app/Controllers/Frontend.php
+++ app/Controllers/Frontend.php
@@ -18,6 +18,7 @@ class Frontend extends BaseController {
     public function login(){
         $error = $this->request->getGet("error");
         $user = new User();
+        $data['isLogged'] = ($user->user_exists($this->session->email) && !empty($this->session->email)) ? true : false;
 
         if($user->user_exists($this->session->email) && !empty($this->session->email)){
             return redirect()->to("/admin");
@@ -25,7 +26,7 @@ class Frontend extends BaseController {
 
         $data['error'] = $error;
 
-        echo view("templates/header");
+        echo view("templates/header", $data);
         echo view("pages/frontend/login", $data);
         echo view("templates/footer");        
     }
