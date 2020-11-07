4aec788b2aac6766ab6709b1319531c5824d9817
Aaron Keel <aaron.keel@hotmail.co.uk>
2020-11-07 01:40:51 +0000

    partial inclusions crud

--- app/Controllers/AdminInclusion.php
+++ app/Controllers/AdminInclusion.php
@@ -3,7 +3,14 @@
 namespace App\Controllers;
 
 class AdminInclusion extends BaseController {
-    
+    public function manage(){
+        $user = new User();
+        $data['isLogged'] = ($user->user_exists($this->session->email) && !empty($this->session->email)) ? true : false;
+        $data['inclusions'] = $->findAll();
+        echo view("templates/header", $data);
+        echo view("pages/admin/inclusions/manage", $data);
+        echo view("templates/footer");     
+    }
 }
 
 ?>
