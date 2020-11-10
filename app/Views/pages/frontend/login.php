10e1ed23a7d2b518da565e6be9ac6b5c2ddc5134
Aaron Keel <aaron.keel@hotmail.co.uk>
2020-11-10 21:40:20 +0000

    login font change

--- app/Views/pages/frontend/login.php
+++ app/Views/pages/frontend/login.php
@@ -1,6 +1,6 @@
 <div class="container h-100">
     <?php if($error === 1 || $error === '1'): ?>
-        <div class="row pt-4 pb-4">
+        <div class="row pt-4 pb-4 trend_sansone">
         <div class="col-sm-12 pt-4">
             <div class="alert alert-dismissible alert-warning">
                 <button type="button" class="close" data-dismiss="alert">&times;</button>
@@ -13,11 +13,11 @@
     <div class="row h-100 justify-content-center align-items-center pt-4 pb-4">
         <form class="col-sm-12" method="POST">
             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <label for="exampleInputEmail1" class="trend_sansone">Email address</label>
+                <input type="email" name="email" class="form-control trend_sansone" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-            <div class="form-group">
+            <div class="form-group trend_sansone">
                 <label for="password1">Password</label>
                 <input type="password" class="form-control" id="password1" name="password" aria-describedby="emailHelp" placeholder="Enter password">
             </div>
