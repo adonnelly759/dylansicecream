84113d835cceba93c71f4b23927fa7d9d203c706
Aaron Keel <aaron.keel@hotmail.co.uk>
2020-11-12 05:11:14 +0000

    retrieve.php printing order details upon unique code search (needs styling)

--- app/Views/pages/frontend/retrieve.php
+++ app/Views/pages/frontend/retrieve.php
@@ -3,7 +3,6 @@
         <h2 class="make_ice_cream_h2  trend_sansone">Retrieve Your Sundae</h2>
     </div>
 </div>
-
 <?php if($error === 1 || $error === '1'): ?>
         <div class="row pb-4 trend_sansone">
         <div class="col-sm-12 pt-4">
@@ -25,24 +24,69 @@
         </form>  
     </div>
     <?php if($codeFound === 1 || $codeFound === '1'): ?>
-        <div class = "row pt-4 pb-4">
-        <table class="table table-responsive table-hover w-100 d-block d-md-table">
-            <thead>
-                <tr>
-                <th scope="col">Code</th>
-                <th scope="col">Created</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php if(!empty($creations)): ?>
-                <?php foreach($creations as $creation): ?>
-                <tr>
-                    <th scope="row"><?php echo $creation['flavours']; ?></th>
-                    <td><?php echo $creation['created']; ?></td>
-                </tr>
-                <?php endforeach; ?>
-            <?php endif; ?>
-            </tbody>
-        </table>
-        </div>
+        <?php if(!empty($creations)): ?>
+            <div class = "row pt-4 pb-4">
+                <div class = "col-sm-12">
+                    <h3 class = "pb-2">Sundae Details</h3>
+                    <p class="sundae-details-bold">Code:&nbsp;</p>
+                    <p><?php echo $creations['code']; ?></p>
+                    <p class="sundae-details-bold">Flavour(s):&nbsp;</p>
+                    <?php if(!empty($creations['flavours'])): ?>
+                        <p>
+                        <?php foreach($creations['flavours'] as $flavour): ?>
+                            <?php echo $flavour['name'] ?>&nbsp;
+                        <?php endforeach; ?>
+                        </p>
+                    <?php else: ?>
+                        <p>No Flavour Chosen</p>
+                    <?php endif;?>
+                    <p class="sundae-details-bold">Inclusion(s):&nbsp;</p>
+                    <?php if(!empty($creations['inclusions'])): ?>
+                        <p>
+                        <?php foreach($creations['inclusions'] as $inclusion): ?>
+                            <?php echo $inclusion['name'] ?>&nbsp;
+                        <?php endforeach; ?>
+                        </p>
+                    <?php else: ?>
+                        <p>No Inclusion Chosen</p>
+                    <?php endif;?>
+                    <p class="sundae-details-bold">Wafer:&nbsp;</p>
+                    <?php if(!empty($creations['wafers'])): ?>
+                        <p>
+                        <?php foreach($creations['wafers'] as $wafer): ?>
+                            <?php echo $wafer['name'] ?>&nbsp;
+                        <?php endforeach; ?>
+                        </p>
+                    <?php else: ?>
+                        <p>No Wafer Chosen</p>
+                    <?php endif;?>
+                    <p class="sundae-details-bold">Sprinkles:&nbsp;</p>
+                    <?php if(!empty($creations['sprinkles'])): ?>
+                        <p>
+                        <?php foreach($creations['sprinkles'] as $sprinkle): ?>
+                            <?php echo $sprinkle['name'] ?>&nbsp;
+                        <?php endforeach; ?>
+                        </p>
+                    <?php else: ?>
+                        <p>No Sprinkles Chosen</p>
+                    <?php endif;?>
+                    <p class="sundae-details-bold">Sauce(s):&nbsp;</p>
+                    <?php if(!empty($creations['sauces'])): ?>
+                        <p>
+                        <?php foreach($creations['sauces'] as $sauce): ?>
+                            <?php echo $sauce['name'] ?>&nbsp;
+                        <?php endforeach; ?>
+                        </p>
+                    <?php else: ?>
+                        <p>No Sauce Chosen</p>
+                    <?php endif;?>
+                    <p class="sundae-details-bold">Cream:&nbsp;</p>
+                    <?php if($creations['cream'] = 1): ?>
+                        <p>Yes</p>
+                    <?php else: ?>
+                        <p>No</p>
+                    <?php endif;?>
+                </div>
+            </div>
+        <?php endif; ?>
     <?php endif; ?>
